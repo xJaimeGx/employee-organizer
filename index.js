59022df24fdf2ffc5293e5e6913193426fb6efb1
xJaimeGx <beanieman019@protonmail.com>
2022-04-30 18:02:10 -0500

    add some pseudocode to index.js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const figlet = require("figlet");
+require("dotenv").config();
+
+
+
+//Create function to connect to database
+
+//Create function to ask user what they would like to do
+
+//Create functions for each user choice
