63eafc734fbbd5fb89d916c9f2ca3705dbac78c9
xJaimeGx <beanieman019@protonmail.com>
2022-04-30 17:07:14 -0500

    populate departments and roles tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,3 +8,27 @@ VALUES
   ("Doctor", "Strange", 3, 2);
 
 SELECT * FROM employees;
+
+INSERT INTO departments (name)
+VALUES
+  ("Mercenary"),
+  ("Espionage"),
+  ("Intelligence"),
+  ("Trainee");
+
+SELECT * FROM departments;
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+  ("Commander", 750000, 1),
+  ("Sergeant", 500000, 1),
+  ("Private", 250000, 1),
+  ("Executive", 600000, 2),
+  ("Agent", 400000, 2),
+  ("Recon", 150000, 2),
+  ("Head Professor", 500000, 3),
+  ("Lead Scientist", 250000, 3),
+  ("Scientist", 100000, 3),
+  ("Intern", 50000, 4);
+
+SELECT * FROM roles;
